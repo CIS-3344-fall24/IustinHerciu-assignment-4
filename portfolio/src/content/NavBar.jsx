f0189da5d042195110b8e40ff0eef71c04699ae3
iustinherciu2000 <tuq84828@temple.edu>
2024-10-22 17:57:52 -0400

    Update NavBar.jsx
    Created 2 constants that will help with the visual editing of the page.

--- portfolio/src/content/NavBar.jsx
+++ portfolio/src/content/NavBar.jsx
@@ -1,10 +1,18 @@
 // This page will include the Navigation Bar
+// import `useEffect` and `useRef` to attach and detach event listeners,
+// and to reference DOM elements directly in React.
+import React, { useEffect, useRef } from 'react';
 
 export default function NavBar() {
+
+    // declare and initialize a `section reference` that will act as the tag for the sections of the page
+    // declare and initialize a `link reference` that will act as the tag for all the links in the navigation bar
+    // use `useRef` to initialize the references
+    const secRef = useRef([]);
+    const linkRef = useRef([]);
+
     return (
         <nav className="navbar">
-            {/* link that when click will take you back to the home page */}
-
 
             {/* create list that will include the navbar options. use unordered list */}
             <header className="header">
