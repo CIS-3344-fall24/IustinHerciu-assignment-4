54ea74aeb52776f2bce0dd641a6ac2bfc9769c1b
iustinherciu2000 <tuq84828@temple.edu>
2024-10-22 22:59:47 -0400

    Update Projects.jsx
    - The page itself is done, I just need to add links and icons

--- portfolio/src/content/Projects.jsx
+++ portfolio/src/content/Projects.jsx
@@ -1,3 +1,61 @@
 export default function Projects() {
-    return <h1>h144</h1>
+    return (
+        <section className="projects" id="projects">
+            <h2 className="heading">
+                <span>Projects</span>
+            </h2>
+
+            <div className="projects-container">
+                <div className="projects-box">
+                    <i className='bx bx-code-alt'></i>
+                    <h3>Add Project Name</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Beatae consectetur doloremque dolorum minima nihil odit quis
+                        recusandae reprehenderit vel voluptatem!</p>
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a href="#" className="btn">Read More</a>
+                </div>
+                <div className="projects-box">
+                    <i className='bx bx-code-alt'></i>
+                    <h3>Add Project Name</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Beatae consectetur doloremque dolorum minima nihil odit quis
+                        recusandae reprehenderit vel voluptatem!</p>
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a href="#" className="btn">Read More</a>
+                </div>
+                <div className="projects-box">
+                    <i className='bx bx-code-alt'></i>
+                    <h3>Add Project Name</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Beatae consectetur doloremque dolorum minima nihil odit quis
+                        recusandae reprehenderit vel voluptatem!</p>
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a href="#" className="btn">Read More</a>
+                </div>
+                <div className="projects-box">
+                    <i className='bx bx-code-alt'></i>
+                    <h3>Add Project Name</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Beatae consectetur doloremque dolorum minima nihil odit quis
+                        recusandae reprehenderit vel voluptatem!</p>
+                    <a href="#" className="btn">Read More</a>
+                </div>
+                <div className="projects-box">
+                    <i className='bx bx-code-alt'></i>
+                    <h3>Add Project Name</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Beatae consectetur doloremque dolorum minima nihil odit quis
+                        recusandae reprehenderit vel voluptatem!</p>
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a href="#" className="btn">Read More</a>
+                </div>
+            </div>
+        </section>
+    );
 }
