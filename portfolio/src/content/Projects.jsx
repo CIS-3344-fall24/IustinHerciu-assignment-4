0342bce105a31eb9f0fc0e474c296cc404bb241b
iustinherciu2000 <tuq84828@temple.edu>
2024-10-23 22:04:35 -0400

    Update Projects.jsx
    - Added the first project

--- portfolio/src/content/Projects.jsx
+++ portfolio/src/content/Projects.jsx
@@ -10,19 +10,23 @@ export default function Projects() {
                     <i className='bx bx-code-alt'></i>
                     <h3>BlackJack Game</h3>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                        Beatae consectetur doloremque dolorum minima nihil odit quis
-                        recusandae reprehenderit vel voluptatem!</p>
+                        A basic card game developed in Java where players aim to get
+                        as close to 21 as possible without exceeding it. The project
+                        features simple game logic and user interaction, making it
+                        an easy-to-play, console-based game.
+                    </p>
+                    <p>
+                        <strong>Tech Style: Java</strong>
+                    </p>
                     {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                    <a href="#" className="btn">GitHub Link</a>
+                    <a href="https://github.com/iustinherciu2000/BlackJack" className="btn" target={"_blank"} rel="noreferrer">GitHub Link</a>
                 </div>
                 <div className="projects-box">
                     <i className='bx bx-code-alt'></i>
                     <h3>Add Project Name</h3>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                        Beatae consectetur doloremque dolorum minima nihil odit quis
-                        recusandae reprehenderit vel voluptatem!</p>
+
+                    </p>
                     {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                     <a href="#" className="btn">Read More</a>
                 </div>
