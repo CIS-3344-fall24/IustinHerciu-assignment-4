41cbd3b23c00a73474271e1231b77eeae146eb72
iustinherciu2000 <tuq84828@temple.edu>
2024-10-23 23:50:51 -0400

    Contact.jsx Update
    - You are now able to send me a message to my email through my portfolio website. I used 'emailjs' in order to achieve this.
    - I also added a message that will appear after you click Send Message

--- portfolio/src/content/Contact.jsx
+++ portfolio/src/content/Contact.jsx
@@ -1,27 +1,64 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 export default function Contact() {
+    const form = useRef();
+    const [messageSent, setMessageSent] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_ou0q9fi', 'template_yhtcwsz', form.current, {
+                publicKey: 'F5MSoIoySbXe7rrcW',
+            })
+            .then(
+                () => {
+                    setMessageSent(true);
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+
     return (
         <section className="contact" id="contact">
             <h2 className="heading">Contact <span>ME</span></h2>
 
-            <form action="#">
+            <form ref={form} onSubmit={sendEmail}>
                 <div className="input-box">
-                    <input type={"text"} placeholder={"Full Name"} />
-                    <input type={"email"} placeholder={"Email Address"} />
+                    <input type={"text"}
+                            name={"to_name"}
+                           placeholder={"Full Name"}/>
                 </div>
 
                 <div className="input-box">
-                    <input type={"number"} placeholder={"Phone Number"} />
-                    <input type={"text"} placeholder={"Email Subject"} />
+                    <input type={"email"}
+                           name={"from_name"}
+                           placeholder={"Email Address"}
+                           required/>
                 </div>
 
-                <textarea name={""}
-                          id={""}
-                          cols="30"
-                          rows="10"
-                          placeholder={"Your Message"}>
-                </textarea>
+                <div className="input-box">
+                    <input type={"text"}
+                           name={"subject"}
+                           placeholder={"Email Subject"}/>
+                </div>
 
-                <input type={"submit"} value="Send Message" className={"btn"} />
+                <div className="input-box">
+                    <textarea name={"message"}
+                              cols="30"
+                              rows="10"
+                              placeholder={"Your Message"}>
+                    </textarea>
+                </div>
+
+                {/* Conditionally render the success message */}
+                {messageSent && <p className="success-message">Your message has been sent successfully!</p>}
+
+                <input type={"submit"} value="Send Message" className={"btn"}/>
 
             </form>
         </section>
